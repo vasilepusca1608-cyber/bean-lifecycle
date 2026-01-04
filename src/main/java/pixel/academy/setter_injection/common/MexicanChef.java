f7a011aced0037ef4aa8ca1bb6708d78f3e26421
Pusca Vasile <vasilepusca1608@gmail.com>
2026-01-04 13:30:03 +0200

    New benul

--- src/main/java/pixel/academy/setter_injection/common/MexicanChef.java
+++ src/main/java/pixel/academy/setter_injection/common/MexicanChef.java
@@ -1,5 +1,7 @@
 package pixel.academy.setter_injection.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -7,6 +9,15 @@ public class MexicanChef implements Chef {
 
     public MexicanChef() {
         System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+    @PostConstruct
+    public void initializeBean() {
+        System.out.println("Bean initialized." + getClass().getSimpleName());
+    }
+
+    @PreDestroy
+    public void cleanup() {
+        System.out.println("Bean is about to be destroyed." + getClass().getSimpleName());
     }
 
     @Override
